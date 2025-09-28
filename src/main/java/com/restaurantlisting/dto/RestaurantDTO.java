a90bee039e969d702758578c8c0a11f702eac412
123SHANIDENGRE <shanidengre@gmail.com>
2025-09-28 15:09:15 +0530

    added UnittestCases

--- src/main/java/com/restaurantlisting/dto/RestaurantDTO.java
+++ src/main/java/com/restaurantlisting/dto/RestaurantDTO.java
@@ -5,9 +5,8 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-@Data
-@AllArgsConstructor
-@NoArgsConstructor
+import java.util.Objects;
+
 
 public class RestaurantDTO {
     private int id;
@@ -15,4 +14,66 @@ public class RestaurantDTO {
     private String address;
     private String city;
     private String restaurantDescription;
+
+    public RestaurantDTO(int id, String name, String address, String city, String restaurantDescription) {
+        this.id = id;
+        this.name = name;
+        this.address = address;
+        this.city = city;
+        this.restaurantDescription = restaurantDescription;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getRestaurantDescription() {
+        return restaurantDescription;
+    }
+
+    public void setRestaurantDescription(String restaurantDescription) {
+        this.restaurantDescription = restaurantDescription;
+    }
+
+    public RestaurantDTO() {
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (!(o instanceof RestaurantDTO that)) return false;
+        return id == that.id && Objects.equals(name, that.name) && Objects.equals(address, that.address) && Objects.equals(city, that.city) && Objects.equals(restaurantDescription, that.restaurantDescription);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name, address, city, restaurantDescription);
+    }
 }
--- src/main/java/com/restaurantlisting/entity/Restaurant.java
+++ src/main/java/com/restaurantlisting/entity/Restaurant.java
@@ -8,10 +8,9 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.util.Objects;
+
 @Entity
-@Data
-@AllArgsConstructor
-@NoArgsConstructor
 public class Restaurant {
 
     @Id
@@ -21,4 +20,66 @@ public class Restaurant {
     private String address;
     private String city;
     private String restaurantDescription;
+
+    public Restaurant(int id, String name, String address, String city, String restaurantDescription) {
+        this.id = id;
+        this.name = name;
+        this.address = address;
+        this.city = city;
+        this.restaurantDescription = restaurantDescription;
+    }
+
+    public Restaurant() {
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getRestaurantDescription() {
+        return restaurantDescription;
+    }
+
+    public void setRestaurantDescription(String restaurantDescription) {
+        this.restaurantDescription = restaurantDescription;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (!(o instanceof Restaurant that)) return false;
+        return id == that.id && Objects.equals(name, that.name) && Objects.equals(address, that.address) && Objects.equals(city, that.city) && Objects.equals(restaurantDescription, that.restaurantDescription);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name, address, city, restaurantDescription);
+    }
 }
